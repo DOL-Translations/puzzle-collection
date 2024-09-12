1b676d0b6f72b8017e0ea59a4d70b88099b8aafb
Philblue7 <Phillipos2001@gmail.com>
2024-09-12 17:33:04 -0400

    Update System.asm

--- src/YCookie/System.asm
+++ src/YCookie/System.asm
@@ -1,8 +1,52 @@
+//NAMES #1
+Text($00047B64, "MARI")
+Text($00047ABC, "YOSH")
+Text($00047AC8, "PEAC")
+Text($00047AD4, "BOWS")
+
 //SAVING SCREEN
-Text($00047B64, "Do not touch the Memory Card\nor the POWER button.")
+Text($00047B64, "Do not touch the Memory Card<n__or the POWER button.")
 
 //DEMO PLAY
 Text($00048200, "DEMO PLAY")
+
+//BUTTONS #1
+Text($0004846C, "Continue")
+Text($00048478, "Quit")
+
+Text($00048480, "Want to save?")
+
+Text($00048494, "Yes")
+Text($0004849C, "No")
+
+Text($000484A4, "Save & Continue")
+
+//BUTTONS #2
+Text($000485F0, "Continue")
+Text($000485FC, "Quit")
+
+//NAMES #2
+Text($000494E8, "YOSH")
+Text($000494F4, "MARI")
+Text($00049500, "PEAC")
+Text($0004950C, "BOWS")
+
+//NAME SCREEN #1
+Text($00049D0C, "NEW")
+
+//PLAYERS
+Text($00049EE0, "Nintendo")
+Text($00049EEC, "COM1")
+Text($00049EF8, "COM2")
+Text($00049F04, "COM3")
+Text($00049F10, "GST1")
+Text($00049F1C, "GST2")
+Text($00049F28, "GST3")
+Text($00049F34, "GST4")
+
+//NAME SCREEN #2
+Text($0004A230, "Is")
+Text($0004A234, "correct?")
 
 //OPTIONS
 Text($001B7A40, "View Records")
@@ -10,7 +54,6 @@ Text("Delete Records")
 Text("Name Entry")
 
 Text($001B7A78, "BGM Vol.")
-
 Text($001B7A8C, "SE Vol.")
 Text("BGM")
 Text("SE")
@@ -36,17 +79,13 @@ Text($001B7B7C, "Chef Mario Outfit")
 Text($001B7B98, "Super Hard COM")
 
 Text($001B7BBC, "Mono")
-
 Text($001B7BC8, "Stereo")
 
 Text($001B7BDC, "1")
-
 Text($001B7BE4, "2")
-
 Text($001B7BEC, "3")
 
 Text($001B7BF8, "OFF")
-
 Text($001B7C00, "ON")
 
 //OTHER
@@ -64,13 +103,11 @@ Text($001B7C50, "Next Stage")
 Text($001B7C6C, "Continue?")
 
 Text($001B7C88, "Yes")
-
 Text($001B7C90, "No")
 
 Text($001B7C9C, "Want to save?")
 
 Text($001B7CB0, "Yes")
-
 Text($001B7CB8, "No")
 
 Text($001B7CC4, "Story")
@@ -154,6 +191,7 @@ Text($001B8550, "participating.")
 
 Text($001B8594, "Learn the basic")
 Text($001B85A8, "rules and techniques.")
+Text($001B85C0, "")
 
 Text($001B85CC, "Learn the rules and")
 Text($001B85E4, "techniques of battle")
@@ -193,3 +231,40 @@ Text($001B88AC, "all data at once.")
 Text($001B88C4, "Is that OK?")
 
 Text($001B88D8, "Select a record to view.")
+
+//HOW TO PLAY (VS. RULES)
+Text($001B8E30, "Learn the rules<n__of playing a<n__battle match.")
+
+//CHARACTERS
+Text($001BB040, "<c0_Mario")
+Text($001BB04C, "<c1_Yoshi")
+Text($001BB05C, "<c2_Bowser")
+Text($001BB068, "<c3_Galoomba")
+Text($001BB078, "<c4_K.Troopa")
+Text($001BB088, "<c5_Buzzy")
+Text($001BB094, "<c3_Galoombas")
+Text($001BB0A8, "<c6_C.Cheep")
+Text($001BB0B8, "<c7_Blooper")
+Text($001BB0C8, "<c8_R.V.Fish")
+Text($001BB0D8, "<c6_C.Cheeps")
+Text($001BB0EC, "<c9_Paratroopa")
+Text($001BB0FC, "<cA_B.Bill")
+Text($001BB108, "<cB_Lakitu")
+Text($001BB118, "<c9_Paratroopas")
+Text($001BB12C, "<cC_Boo")
+Text($001BB138, "<cD_Eerie")
+Text($001BB14C, "<cE_F.Boo")
+Text($001BB15C, "<cC_Boos")
+Text($001BB16C, "<cF_Spiny")
+Text($001BB17C, "<cG_Piranha Plant")
+Text($001BB194, "<cH_Hammer Bro")
+Text($001BB1A8, "<cF_Spinys")
+Text($001BB1BC, "<cI_Thwomp")
+Text($001BB1CC, "<cJ_Kamek")
+Text($001BB1DC, "<cY_Toads")
+Text($001BB1F0, "<cP_Peach")
+Text($001BB200, "<cE_Fishing Boos")
+Text($001BB214, "<cH_Hammer Bros.")
+
+//STORY
+Text($001BB2B8, "Tomorrow is Peach's<n__birthday.")
