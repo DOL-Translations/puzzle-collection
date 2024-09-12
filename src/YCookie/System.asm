15ea1fabe21d0b435476915eda756900b4a24217
Philblue7 <Phillipos2001@gmail.com>
2024-09-12 16:46:41 -0400

    Update System.asm

--- src/YCookie/System.asm
+++ src/YCookie/System.asm
@@ -1,3 +1,6 @@
+//SAVING SCREEN
+Text($00047B64, "Do not touch the Memory Card\nor the POWER button.")
+
 //DEMO PLAY
 Text($00048200, "DEMO PLAY")
 
@@ -75,44 +78,100 @@ Text($001B7CC4, "Story")
 Text($001B7CD8, "Next Stage")
 
 //CONGRATULATIONS
-Text($001B7D20, "Congratulations!\nYou did a great job\ncoming this far!\nIn CLASSIC mode, set ROUND to 10,\nthen press the D-Pad right.\nYou'll be able to play ROUND 11\nand onward.")
+Text($001B7D20, "Congratulations!")
+Text($001B7D48, "You did a great job")
+Text($001B7D6C, "coming this far!")
+Text($001B7D94, "In CLASSIC mode, set ROUND to 10,")
+Text($001B7DB8, "then press the D-Pad right.")
+Text($001B7DE0, "You'll be able to play ROUND 11")
+Text($001B7E08, "and onward.")
 
-Text($001B7E4C, "Congratulations!\nYou did a great job\ncoming this far!\nYou can now adjust the\n"Smooth Animation" and\n"Chef Mario Outfit" settings\nin the OPTIONS menu.")
+Text($001B7E4C, "Congratulations!")
+Text($001B7E74, "You did a great job")
+Text($001B7E98, "coming this far!")
+Text($001B7EC0, "You can now adjust the")
+Text($001B7EE4, ""Smooth Animation" and")
+Text($001B7F04, ""Chef Mario Outfit" settings")
+Text($001B7F2C, "in the OPTIONS menu.")
 
-Text($001B7F78, "Congratulations!\nYou did a great job\nbeating EASY!\nNow try NORMAL next!")
+Text($001B7F78, "Congratulations!")
+Text($001B7FA0, "You did a great job")
+Text($001B7FC4, "beating EASY!")
+Text($001B7FE0, "Now try NORMAL next!")
 
-Text($001B8030, "Congratulations!\nYou did a great job\nbeating NORMAL!\nNow try HARD next!")
+Text($001B8030, "Congratulations!")
+Text($001B8058, "You did a great job")
+Text($001B8078, "beating NORMAL!")
+Text($001B8094, "Now try HARD next!")
 
-Text($001B80E4, "Congratulations!\nYou did a great job\nbeating HARD!\nYou can now adjust the\n"Super Hard COM" setting\nin the OPTIONS menu.\nThis will make your opponents\nstronger in MULTIPLAYER mode.")
+Text($001B80E4, "Congratulations!")
+Text($001B810C, "You did a great job")
+Text($001B8130, "beating HARD!")
+Text($001B8158, "You can now adjust the")
+Text($001B817C, ""Super Hard COM" setting")
+Text($001B819C, "in the OPTIONS menu.")
+Text($001B81C4, "This will make your opponents")
+Text($001B81EC, "stronger in MULTIPLAYER mode.")
 
 //MAIN MENU
-Text($001B822C, "Play all by yourself.\nYou can also look at\nBASIC RULES under\nHOW TO PLAY.")
+Text($001B822C, "Play all by yourself.")
+Text($001B8248, "You can also look at")
+Text($001B8264, "BASIC RULES under")
+Text($001B8280, "HOW TO PLAY.")
 
-Text($001B8290, "Play against a friend.\nYou can also look at\nVS RULES under\nHOW TO PLAY.")
+Text($001B8290, "Play against a friend.")
+Text($001B82AC, "You can also look at")
+Text($001B82C8, "VS RULES under")
+Text($001B82DC, "HOW TO PLAY.")
 
-Text($001B82F4, "Play against others.\nYou can also look at\nVS RULES under\nHOW TO PLAY.")
+Text($001B82F4, "Play against others.")
+Text($001B8310, "You can also look at")
+Text($001B832C, "VS RULES under")
+Text($001B8340, "HOW TO PLAY.")
 
-Text($001B8358, "Learn the rules of\nand techniques of\nYoshi's Cookie.")
+Text($001B8358, "Learn the rules of")
+Text($001B8374, "and techniques of")
+Text($001B838C, "Yoshi's Cookie.")
 
-Text($001B83A0, "Adjust various game\nsettings. More options\nwill appear once some\nconditions are fulfilled.")
+Text($001B83A0, "Adjust various game")
+Text($001B83B8, "settings. More options")
+Text($001B83D4, "will appear once some")
+Text($001B83F0, "conditions are fulfilled.")
 
-Text($001B843C, "Play a standard game\nand match up all\nof the cookies to\nclear the stage.")
+Text($001B843C, "Play a standard game")
+Text($001B8454, "and match up all")
+Text($001B8468, "of the cookies to")
+Text($001B8480, "clear the stage.")
 
-Text($001B849C, "Experience the whole\nstory while trying to\nrecover the stolen\ncookies.")
+Text($001B849C, "Experience the whole")
+Text($001B84B4, "story while trying to")
+Text($001B84CC, "recover the stolen")
+Text($001B84E4, "cookies.")
 
-Text($001B8520, "Please select the\nnumber of players\nparticipating.")
+Text($001B8520, "Please select the")
+Text($001B8534, "number of players")
+Text($001B8550, "participating.")
 
-Text($001B8594, "Learn the basic\nrules and techniques.")
+Text($001B8594, "Learn the basic")
+Text($001B85A8, "rules and techniques.")
 
-Text($001B85CC, "Learn the rules\nand techniques of battle\nmatches.")
+Text($001B85CC, "Learn the rules and")
+Text($001B85E4, "techniques of battle")
+Text($001B85FC, "matches.")
 
-Text($001B8628, "Please select your name.\nIf you're new, choose\nNEW or Guest.")
+Text($001B8628, "Please select your name.")
+Text($001B8644, "If you're new, choose")
+Text($001B8660, "NEW or Guest.")
 
 Text($001B8680, "Return to the main menu.")
 
-Text($001B86A8, "Guests can play\nwithout having to\nsave records.")
+Text($001B86A8, "Guests can play")
+Text($001B86BC, "without having to")
+Text($001B86D4, "save records.")
 
-Text($001B86F8, "Please select your name.\nIf you're new, choose\nNEW or Guest.")
+Text($001B86F8, "Please select your name.")
+Text($001B8714, "If you're new, choose")
+Text($001B8730, "NEW or Guest.")
 
 Text($001B8780, "Please choose a name.")
 
@@ -120,12 +179,17 @@ Text($001B87A8, "Return to OPTIONS menu.")
 
 Text($001B87CC, "Change name?")
 
-Text($001B8804, "Select the data\nyou want to delete.")
+Text($001B8804, "Select the data")
+Text($001B8818, "you want to delete.")
 
 Text($001B8834, "Return to OPTIONS menu.")
 
-Text($001B8858, "Deleting this\nname will erase\nits data. Is that OK?")
+Text($001B8858, "Deleting this")
+Text($001B8868, "name will erase")
+Text($001B887C, "its data. Is that OK?")
 
-Text($001B8898, "This will erase\nall data at once.\nIs that OK?")
+Text($001B8898, "This will erase")
+Text($001B88AC, "all data at once.")
+Text($001B88C4, "Is that OK?")
 
 Text($001B88D8, "Select a record to view.")
